19e540bd3aaf078ed4316d4a24f682b5cca59917
MarcelaDm4 <157128334+MarcelaDm4@users.noreply.github.com>
2024-03-01 08:12:17 -0600

    Add files via upload

---
+++ Semana 7/Laboratorio/L7BMDM1093924.py
@@ -0,0 +1,44 @@
+#print("hola mundo")
+#print("Ingrese su nombre: ")
+#nombre = input()
+# print("Mi nombre es: "+nombre)
+
+#Ejercicio 1
+print("Ingrese su nombre")
+nombre = input()
+print("Hola "+nombre+", buen inicio de semana!")
+
+#Ejercicio 2
+print("Hola, ingresa tu nombre")
+nombre = input()
+print("Ahora ingresa tu edad")
+edad = int(input())
+print("Hola !"+nombre+"¡, tienes "+str(edad)+" años de edad")
+
+#Ejercicio 3
+print("Ingresa la temperatua en celsius")
+C = int(input())
+F = C*9/5+32
+print("La temperatura en Farenheit es: "+str(F))
+
+#Ejercicio 4
+print("Ingrese num 1")
+num1 = int(input())
+print("Ingrese num 2")
+num2 = int(input())
+suma = num1+num2
+print("La suma de sus números es de "+str(suma))
+resta = num1-num2
+print("La resta de sus números es de "+str(resta))
+multiplicacion = num1*num2
+print("El producto de sus números es de "+str(multiplicacion))
+division = num1/num2
+print("La división de sus números es de "+str(division))
+
+#Ejercicio 5
+print("Ingrese la altura del triángulo")
+H = int(input())
+print("Ingrese la base del triángulo")
+B = int(input())
+area=B*H/2
+print("El área de si triángulo es de "+str(area))
